1178c3974d458ba196e1f11f975b9caf0a49b342
Patrick Coglianese <patrick.coglianese@gmail.com>
2022-07-27 10:09:21 -0400

    Create specialsconfig.js

---
+++ specialsconfig.js
@@ -0,0 +1,22 @@
+{
+  monday:{
+   bridget:'Music',
+   calvin:'PE',
+  },
+  tuesday:{
+   bridget:'PE',
+   calvin:'Library',
+  },
+  wedneday:{
+   bridget:'Library',
+   calvin:'PE',
+  },
+  thursday:{
+   bridget:'PE',
+   calvin:'Art',
+  },
+  friday:{
+   bridget:'Library',
+   calvin:'Music',
+  }
+}
