ab968e0289ae3ab312492f5b4b13cc88413e1f7d
Patrick Coglianese <patrick.coglianese@gmail.com>
2024-07-01 13:40:09 -0400

    Update summer24.js
    added dates

--- summer24.js
+++ summer24.js
@@ -41,6 +41,59 @@
 			"when" : "M-F with Th sleepover!"
 			}
 			]
+		},
+		"starts" : "07/14/2024",
+		"ends" : "07/20/2024",
+		"activities" : [
+			]
+		},
+		"starts" : "07/21/2024",
+		"ends" : "07/27/2024",
+		"activities" : [
+			{
+			"who" : "Bridget",
+			"what" : "Soccer Camp",
+			"where" : "Westborough HS",
+			"when" : "M-F 9:00 - 12:00am"
+			}
+			]
+		},
+		"starts" : "07/28/2024",
+		"ends" : "08/03/2024",
+		"activities" : [
+			{
+			"who" : "Family",
+			"what" : "Pippa & Grandpa Vacation",
+			"where" : "Simsbury, CT",
+			"when" : "Sunday - Saturday"
+			}
+			]
+		},
+		"starts" : "08/04/2024",
+		"ends" : "08/10/2024",
+		"activities" : [
+			]
+		},
+		"starts" : "08/01/2024",
+		"ends" : "08/17/2024",
+		"activities" : [
+			]
+		},
+		"starts" : "08/18/2024",
+		"ends" : "08/24/2024",
+		"activities" : [
+			{
+				"who" : "Family",
+				"what" : "Rye Beach Vacation",
+				"where" : "Rye, NH",
+				"when" : "Sunday - Saturday"
+			}
+			]
+		},
+		"starts" : "08/25/2024",
+		"ends" : "08/31/2024",
+		"activities" : [
+			]
 		}
 	]
 }
