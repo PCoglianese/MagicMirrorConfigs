211b2dede557871be433900ce2aaefeba0b9244a
Patrick Coglianese <patrick.coglianese@gmail.com>
2024-07-01 10:56:16 -0400

    Create summer24.js

---
+++ summer24.js
@@ -0,0 +1,32 @@
+{
+	"summer": [
+		"week": {
+      "dates":"6/30 - 7/6",
+      "activities" : [
+          {
+            "who" : "Family",
+            "what" : "Kelleher Pool Party!",
+            "where" : "Kelleher Pool",
+            "when" : "4th of July"
+          }
+        ]
+		},
+		"week": {
+      "dates":"7/7 - 7/13",
+      "activities" : [
+        {
+          "who" : "Calvin",
+          "what" : "Nike Soccer Camp",
+          "where" : "Fore Kicks Marlborough",
+          "when" : "M-F 8:30am - 11:30am"
+        },
+        {
+          "who" : "Bridget",
+          "what" : "Girl Scout Camp",
+          "where" : "Girl Scout Camp",
+          "when" : "M-F with Th sleepover!"
+        }
+        ]
+		}
+	]
+}
